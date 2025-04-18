3f8ea4652c1b703777f37f4ec43e7d6c307dfc0b
kpengk@qq.com <kangpeng@glasssix.com>
2025-04-18 18:25:19 +0800

    perf: Optimizing competition between tasks.

--- include/SequentialThreadPool.hpp
+++ include/SequentialThreadPool.hpp
@@ -12,6 +12,20 @@
 #include <unordered_map>
 #include <vector>
 
+namespace detail {
+    template <typename Container, typename UnaryPredicate>
+    auto find_if_from(Container& container, typename Container::iterator start, UnaryPredicate pred) {
+        // [start, end)
+        auto it = std::find_if(start, container.end(), pred);
+        if (it != container.end()) {
+            return it;
+        }
+
+        // [begin, start)
+        it = std::find_if(container.begin(), start, pred);
+        return it == start ? container.end() : it;
+    }
+} // namespace detail
 
 enum class TaskReply { done, retry };
 class SequentialThreadPool;
@@ -38,7 +52,7 @@ private:
 
 class SequentialThreadPool {
 public:
-    explicit SequentialThreadPool(size_t threads) : task_id_{}, stop_token_{} {
+    explicit SequentialThreadPool(size_t threads) : wait_task_count_{}, task_id_{}, stop_token_{} {
         for (size_t i = 0; i < threads; ++i) {
             workers_.emplace_back([this]() { run(); });
         }
@@ -57,20 +71,20 @@ public:
     template <typename T,
         typename std::enable_if<std::is_same<decltype(std::declval<T>()({})), TaskReply>::value>::type* = nullptr>
     void post(uint32_t group, T&& task) {
-        bool waiting{}; // The task group is waiting for new tasks
+        bool waiting{}; // Waiting for new tasks.
         {
             std::lock_guard<std::mutex> lock(mutex_);
             if (stop_token_) {
                 throw std::runtime_error("enqueue on stopped thread pool");
             }
-            waiting = std::all_of(
-                ready_group_.cbegin(), ready_group_.cend(), [&](const uint32_t& val) { return val != group; });
-            group_tasks_[group].emplace_back(
-                Task{task_id_, waiting ? TaskStatus::ready : TaskStatus::wait, std::forward<T>(task)});
-            ++task_id_;
-            if (waiting) {
-                ready_group_.push_back(group);
+            if (group_tasks_.find(group) == group_tasks_.end()) {
+                const auto res = group_tasks_.emplace(group, std::deque<Task>());
+                next_exec_group_ = res.first;
             }
+            group_tasks_[group].emplace_back(Task{false, task_id_, std::forward<T>(task)});
+            waiting = wait_task_count_ == 0;
+            ++task_id_;
+            ++wait_task_count_;
         }
         if (waiting) {
             condition_.notify_one();
@@ -121,7 +135,7 @@ public:
 
     void wait_for_done() {
         {
-            std::unique_lock<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mutex_);
             stop_token_ = true;
         }
         condition_.notify_all();
@@ -135,30 +149,17 @@ private:
     void run() {
         while (true) {
             std::unique_lock<std::mutex> lock(mutex_);
-            condition_.wait(lock, [this]() { return !ready_group_.empty() || stop_token_; });
-            if (ready_group_.empty() && stop_token_) {
+            condition_.wait(lock, [this]() { return wait_task_count_ != 0 || stop_token_; });
+            if (wait_task_count_ == 0 && stop_token_) {
                 break;
             }
             // Pull tasks from task group.
-            const uint32_t group = ready_group_.front();
-            ready_group_.pop_front();
-            const auto task_iter = std::find_if(group_tasks_[group].begin(), group_tasks_[group].end(),
-                [](const Task& task) { return task.status != TaskStatus::running; });
-            assert(task_iter != group_tasks_[group].end());
-            Task& task = *task_iter;
-            assert(task.status == TaskStatus::ready);
-            task.status = TaskStatus::running;
-            // Add the current group of tasks to the ready queue.
-            const auto wait_iter = std::find_if(task_iter + 1, group_tasks_[group].end(),
-                [](const Task& task) { return task.status == TaskStatus::wait; });
-            if (wait_iter != group_tasks_[group].end()) {
-                wait_iter->status = TaskStatus::ready;
-                ready_group_.push_back(group);
-                lock.unlock();
-                condition_.notify_one();
-            } else {
-                lock.unlock();
-            }
+            std::pair<uint32_t, Task&> task_info = find_ready_task();
+            const uint32_t& group = task_info.first;
+            Task& task = task_info.second;
+            task.runing = true;
+            --wait_task_count_;
+            lock.unlock();
             // Execute the task.
             TaskContext ctx(this, group, task.id);
             const TaskReply reply = task.f(&ctx);
@@ -168,12 +169,14 @@ private:
                 lock.lock();
                 group_tasks_[group].pop_front();
                 lock.unlock();
-                // Notify all threads, that some threads are waiting for the previous task to complete.
+                // Wake up the next task in the group that may be waiting.
+                // Notify all threads because multiple tasks in a task group may be waiting,
+                // and waking up only one task cannot determine the wakeup order.
                 condition_.notify_all();
             } else {
                 lock.lock();
-                task.status = TaskStatus::ready;
-                ready_group_.push_back(group);
+                task.runing = false;
+                ++wait_task_count_;
                 lock.unlock();
                 // No "notify_one()" is needed here, because the current thread
                 // will immediately check whether "ready_group_" is empty.
@@ -187,29 +190,48 @@ private:
             if (group_tasks_[group].empty()) {
                 return true;
             }
-            const uint32_t val = group_tasks_[group].front().id;
-            if (val != task) {
-                return false;
-            }
             return group_tasks_[group].front().id == task;
         });
     }
 
     friend class TaskContext;
 
-    enum class TaskStatus { wait, ready, running };
-
     struct Task {
+        bool runing{};
         uint32_t id{};
-        TaskStatus status{};
         std::function<TaskReply(TaskContext*)> f;
     };
 
+    std::pair<uint32_t, Task&> find_ready_task() {
+        // Get the task at the head of the queue to avoid waiting for the previous task to complete.
+        const auto ready_group = detail::find_if_from(group_tasks_, next_exec_group_,
+            [](const auto& item) { return !item.second.empty() && !item.second.front().runing; });
+        if (ready_group != group_tasks_.end()) {
+            next_exec_group_ = std::next(ready_group);
+            return {ready_group->first, ready_group->second.front()};
+        }
+        // Get a task that has not running.
+        while (true) {
+            if (next_exec_group_ == group_tasks_.end()) {
+                next_exec_group_ = group_tasks_.begin();
+            }
+            const auto task_iter = std::find_if(next_exec_group_->second.begin(), next_exec_group_->second.end(),
+                [](const auto& item) { return !item.runing; });
+            if (task_iter != next_exec_group_->second.end()) {
+                const uint32_t group = next_exec_group_->first;
+                ++next_exec_group_;
+                return {group, *task_iter};
+            }
+            ++next_exec_group_;
+        }
+    }
+
     std::vector<std::thread> workers_;
-    std::unordered_map<uint32_t, std::deque<Task>> group_tasks_;
-    std::deque<uint32_t> ready_group_;
     mutable std::mutex mutex_;
     std::condition_variable condition_;
+    std::unordered_map<uint32_t, std::deque<Task>> group_tasks_;
+    std::unordered_map<uint32_t, std::deque<Task>>::iterator next_exec_group_;
+    std::uint32_t wait_task_count_;
     std::uint32_t task_id_;
     bool stop_token_;
 };
@@ -233,6 +255,6 @@ inline uint32_t TaskContext::id() const {
 inline void TaskContext::wait_previous() {
     if (!waited_) {
         pool_->wait_previous(group_, id_);
+        waited_ = true;
     }
-    waited_ = true;
 }
